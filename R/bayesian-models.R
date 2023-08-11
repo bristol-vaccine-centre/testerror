381364ac2ff7af49302073a4b058df154d3895c5
robchallen <rob@terminological.co.uk>
2023-08-11 15:58:07 +0100

    pin windows version

--- R/bayesian-models.R
+++ R/bayesian-models.R
@@ -35,6 +35,25 @@ NULL
   path = fs::path_ext_set(fs::path(rappdirs::user_cache_dir("testerror"),"models",rstan::stan_version(),name),"stan")
   fs::dir_create(fs::path_dir(path))
   if (!fs::file_exists(path)) fs::file_copy(file, path)
+  
+  # Windows fix ----
+  # https://github.com/stan-dev/rstan/wiki/Configuring-C---Toolchain-for-Windows
+  # dotR <- file.path(Sys.getenv("HOME"), ".R")
+  # if (!file.exists(dotR)) dir.create(dotR)
+  # M <- file.path(dotR, "Makevars.win")
+  # if (!file.exists(M)) file.create(M)
+  # cat("\n", #CXX14FLAGS=-O3 -march=native",
+  #     "CXX14=$(BINPREF)g++ -O2 -march=native -mtune=native",
+  #     "CXX14FLAGS=-O3 -march=native -mtune=native",
+  #     "CXX11FLAGS=-O3 -march=native -mtune=native",
+  #     # "CXX14 = g++ -m$(WIN) -std=c++1y",
+  #     # "CXX11FLAGS=-O3 -march=native",
+  #     file = M, sep = "\n", append = TRUE)
+  # CXX14FLAGS=-O3 -march=native -mtune=native
+  # CXX11FLAGS=-O3 -march=native -mtune=native
+  # CXX14 = g++ 
+  # CXX14FLAGS = -O3
+  
   tmp = rstan::stan_model(
     file = path,
     model_name = name,
